653577bf5e68d7a4353d02ed78d324c5e86cbb13
Kumarananya <47805061+Kumarananya@users.noreply.github.com>
2019-08-02 17:52:55 +0530

    Add files via upload

--- finalcheck/js/script.js
+++ finalcheck/js/script.js
@@ -1,52 +1,51 @@
-/// Include truYum form validation functions here
-function nameValidation(){
-	var TitleVal=document.getElementById('name').value;
-                var regex=/^[_A-z]*((-|\s)*[_A-z])*$/g;
-                var isValid=regex.test(val);
-                var msg="";
-                if(TitleVal=="" || TitleVal== null){
+function titleValidation(){
+	var TitleVal = document.getElementById('title').value;
+                var regex = /^[_A-z]*((-|\s)*[_A-z])*$/g;
+                var isValid = regex.test(TitleVal);
+                var msg = "";
+                if(TitleVal == "" || TitleVal == null){
                    msg="Title is required.";
                 }
-                else if(TitleVal.length<2 || TitleVal.length>100){
-                    msg="Title should have 2 to 100 characters.";
+                else if(TitleVal.length < 2 || TitleVal.length > 100){
+                    msg = "Title should have 2 to 100 characters.";
                 }
                 else{
-                    msg="";
-                    c=0;
+                    msg = "";
+                    c = 0;
                 }
                 
-                document.getElementById("name-validation").innerHTML=msg;
+                document.getElementById("title-validation").innerHTML=msg;
 }
 function dateOfLaunch(){
 	
 }
-function grossValidation(){
+function validate(){
 	
-	var val=document.getElementById('Gross').value;
-	var regex=/^[0-9]*$/g;
-                var isValid=regex.test(val);
-                var msg="";
-                if(val=="" || val== null){
-                    msg="Gross is required.&emsp;&emsp;&emsp;&ensp;&ensp;";
+	var grossVal = document.getElementById('gross').value;
+	var regex = /^[0-9]*$/g;
+                var isValid = regex.test(grossVal);
+                var msg = "";
+                if(grossVal == "" || grossVal == null){
+                    msg = "Gross is required.";
                 }
-                else if(isValid==false){
-                    msg="Gross has to be a number.";
+                else if(isValid == false){
+                    msg = "Gross has to be a number.";
                 }
                 else{
-                    msg="";
-                    c=0;
+                    msg = "";
+                    c = 0;
                 }
                 
-                document.getElementById("Gross-validation").innerHTML=msg;
-	val=document.getElementById('dateOfLaunch').value;
+                document.getElementById("gross-validation").innerHTML=msg;
+	dolVal = document.getElementById('dateOfLaunch').value;
 	msg="";
-	if(val=="" || val== null){
-	   msg="Date of Launch is required.";
+	if(dolVal == "" || dolVal == null){
+	   msg = "Date of Launch is required.";
 	}
 	document.getElementById("date-of-launch-validation").innerHTML=msg;
-	var val=document.getElementById('Genre').value;
-	var msg="";
-	if(val=="" || val== null){
+	var genreVal = document.getElementById('genre').value;
+	var msg = "";
+	if(genreVal == "" || genreVal == null){
 	   msg="Select one genre";
 	}
 	document.getElementById("genre-validation").innerHTML=msg;
